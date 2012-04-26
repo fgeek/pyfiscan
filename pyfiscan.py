1713a91c06ed91f7ec325673798cd77d2c016a6d
Henri Salo <henri@nerv.fi>
2012-04-26 20:37:34 +0300

    Added more TODO-items.

--- pyfiscan.py
+++ pyfiscan.py
@@ -12,6 +12,8 @@ Pyfiscan is free web-application vulnerability and version scanner, which is pyt
 Known issues and/or bugs:
 1: If instance is upgraded from Joomla 1.6.1 to 1.7.x by unzipping there will be both version files libraries/joomla/version.php and includes/version.php where first is the old one.
 
+TODO: Strip output --strip-output, which should remove homedir/startdir and location from output (stdin, csv and log)
+TODO: If one fingerprint finds a match the process should finish and not be scanned with other fingerprints
 TODO: There should be argument for looking specific programs in for example: -s joomla,smf
 TODO: There should be --loglevel=<levelname>
 TODO: Add unittests
