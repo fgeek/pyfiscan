7f50c65ae33004d3daca9a9ed7238b7ddca6fe6c
Henri Salo <henri@nerv.fi>
2012-07-31 10:12:40 +0300

    Class does not need to generate data dictionary in init now. This fixed a bug where all files would get read and processed twice Added AttributeError in case YAML-file is empty (probably YAML-files will be empty in case it is under development) Added exception for ImportErrors Docstrings

--- database.py
+++ database.py
@@ -1,20 +1,21 @@
 # -*- coding: utf-8 -*-
 
-import yaml
-import os
-import os.path
-import sys
+try:
+    import yaml
+    import os
+    import sys
+except ImportError, error:
+    print('Import error: %s' % error)
+    sys.exit(1)
 
-class Database():
-    def __init__(self, yamldir):
-        self.data = self.generate(yamldir)
 
+class Database:
+    """Reads YAML files and generates a data dictionary of the contents"""
     def gen_yamlfile_locations(self, yamldir):
+        """File handle generator for YAML-files"""
         if os.path.islink(yamldir):
-            # TODO: logging
             sys.exit('Location for YAML-files can not be a symlink: %s' % yamldir)
         if not os.path.isdir(yamldir):
-            # TODO: logging
             sys.exit('Location for YAML-files is not a directory: %s' % yamldir)
         for filename in os.listdir(yamldir):
             if not filename.endswith('.yaml'):
@@ -27,19 +28,22 @@ class Database():
             yield file
 
     def generate(self, yamldir):
+        """Generates data dictionary of definitions from YAML files"""
         data = {}
         for file in self.gen_yamlfile_locations(yamldir):
             try:
                 result = yaml.safe_load(file.read())
                 data = dict(data.items() + result.items())
-            except yaml.scanner.ScannerError, e:
-                print('Error while loading YAML-file: %s' % filename)
+            except AttributeError, e:  # empty file
+                print('No data found inside: %s' % file)
+            except yaml.scanner.ScannerError, e:  # syntax error
+                print('Error while loading YAML-file: %s' % file)
         return data
 
-    def locations(self, application, with_lists=True):
+    def locations(self, data, application, with_lists=True):
         """Returns list of locations by appname."""
         locations = []
-        for (appname, issues) in self.data.iteritems():
+        for (appname, issues) in data.iteritems():
             if not appname == application:
                 continue
             for issue in issues.iteritems():
