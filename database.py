bb9e234678af3d1b1877df34025920c2ef63e14b
Henri Salo <henri@nerv.fi>
2013-09-08 22:26:08 +0300

    Cleanup.

--- database.py
+++ database.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+"""Provides interface for pyfiscan to YAML-files."""
+
 try:
     import sys
     import os
@@ -9,44 +11,49 @@ except ImportError, error:
     sys.exit(1)
 
 
+def gen_yamlfile_locations(yamldir, includes):
+    """File handle generator for YAML-files. Only used by database class."""
+    if os.path.islink(yamldir):
+        sys.exit('Location for YAML-files can not be a symlink: %s' % yamldir)
+    if not os.path.isdir(yamldir):
+        sys.exit('Location for YAML-files is not a directory: %s' % yamldir)
+    for filename in os.listdir(yamldir):
+        if not filename.endswith('.yml'):  # ignores upcoming fingerprints
+            continue
+        if os.path.islink(yamldir + filename):
+            continue
+        if not os.path.isfile(yamldir + filename):
+            continue
+        if not includes:
+            yield open(yamldir + filename, 'r')
+        else:
+            for item in includes:
+                if filename == item + '.yml':
+                    yield open(yamldir + filename, 'r')
+
+
+def generate(yamldir, includes):
+    """Generates data dictionary of definitions from YAML files. Only used by
+    database class.
+    
+    """
+    data = {}
+    for yamlfile in gen_yamlfile_locations(yamldir, includes):
+        try:
+            result = yaml.safe_load(yamlfile.read())
+            data = dict(data.items() + result.items())
+            yamlfile.close()
+        except AttributeError:  # empty file
+            print('No data found inside: %s' % yamlfile)
+        except yaml.scanner.ScannerError, e:  # syntax error
+            print('Error while loading YAML-file: %s' % e)
+    return data
+
+
 class Database:
     """Reads YAML files and generates a data dictionary of the contents"""
     def __init__(self, yamldir, includes=None):
-        self.issues = self.generate(yamldir, includes)
-
-    def gen_yamlfile_locations(self, yamldir, includes):
-        """File handle generator for YAML-files"""
-        if os.path.islink(yamldir):
-            sys.exit('Location for YAML-files can not be a symlink: %s' % yamldir)
-        if not os.path.isdir(yamldir):
-            sys.exit('Location for YAML-files is not a directory: %s' % yamldir)
-        for filename in os.listdir(yamldir):
-            if not filename.endswith('.yml'):  # ignores upcoming fingerprints
-                continue
-            if os.path.islink(yamldir + filename):
-                continue
-            if not os.path.isfile(yamldir + filename):
-                continue
-            if not includes:
-                yield open(yamldir + filename, 'r')
-            else:
-                for item in includes:
-                    if filename == item + '.yml':
-                        yield open(yamldir + filename, 'r')
-
-    def generate(self, yamldir, includes):
-        """Generates data dictionary of definitions from YAML files"""
-        data = {}
-        for yamlfile in self.gen_yamlfile_locations(yamldir, includes):
-            try:
-                result = yaml.safe_load(yamlfile.read())
-                data = dict(data.items() + result.items())
-                yamlfile.close()
-            except AttributeError:  # empty file
-                print('No data found inside: %s' % yamlfile)
-            except yaml.scanner.ScannerError, e:  # syntax error
-                print('Error while loading YAML-file: %s' % e)
-        return data
+        self.issues = generate(yamldir, includes)
 
     def locations(self, application, with_lists=True):
         """Returns list of locations by appname."""
