06de0466694fad8e6ae23823991d8bd27f4dd333
Henri Salo <henri@nerv.fi>
2013-09-20 13:42:20 +0300

    More details to message. Now emails to several addresses if first row of result CSV contains comma.

--- mailer.py
+++ mailer.py
@@ -34,12 +34,11 @@ if version_minor < int(6):
     sys.exit('Python minor version needs to be seven or higher.\nSMTP_SSL only works with Python 2.7')
 
 
-def send_email(user, appname, version_file, file_version, secure_version, cve):
+def send_email(user, timestamp, appname, version_file, file_version, secure_version, cve):
     """This will handle email sending to SMTP-server."""
-#    msg = MIMEText('Hei, olemme huomanneet, että käytössäsi on ' + appname + '-sovellus, jonka versio ' + file_version + ' on haavoittuvainen. Voit korjata tilanteen päivittämällä asennuksen vähintään versioon ' + secure_version +'. Apua sovelluksen päivittämiseen saat vastaamalla tähän sähköpostiin sekä tästä ohjeesta: http://www.kapsi.fi/ohjeet/www-paivitys.html\n\nHaavoittuva sovellus löytyy hakemistostasi: ' + version_file + '\n\nLisätietoja: ' + cve + '\n\nTähän sähköpostiin ei tarvitse vastata mikäli sinulla ei ole ongelmia päivityksessä. Huomaathan, että myös htaccessin takana olevat sivut suositellaan päivittämään ja keskeneräiset asennukset on asennettava loppuun tai poistettava. Mikäli verkkotunnuksesi on siirretty voit ajaa komennon "chmod 0220 hakemisto", jolloin emme lähetä sinulle enää muistutuksia.\n\nTerveisin,\n   Henri Salo\n   Kapsin ylläpito\n   yllapito@tuki.kapsi.fi\n\n   Kapsi Internet-käyttäjät ry\n   http://www.kapsi.fi/\n   http://tuki.kapsi.fi/', _charset='utf-8')
-    msg = MIMEText('Hei, olemme huomanneet, että käytössäsi on ' + appname + '-sovellus, jonka versio ' + file_version + ' on haavoittuvainen. Voit korjata tilanteen päivittämällä asennuksen vähintään versioon ' + secure_version + '. Apua sovelluksen päivittämiseen saat vastaamalla tähän sähköpostiin.\n\nTietoturva-aukollinen sovellus löytyy hakemistostasi: ' + version_file + '\n\nLisätietoja: ' + cve + '\n\nTähän sähköpostiin ei tarvitse vastata mikäli sinulla ei ole ongelmia päivityksessä.\n\nTerveisin,\n   Henri Salo', _charset='utf-8')
-    to_address = user
+    msg = MIMEText('Hei, olemme huomanneet, että käytössäsi on ' + appname + '-sovellus, jonka versio ' + file_version + ' on haavoittuvainen. Voit korjata tilanteen päivittämällä asennuksen vähintään versioon ' + secure_version +'. Apua sovelluksen päivittämiseen saat vastaamalla tähän sähköpostiin sekä tästä ohjeesta: http://www.kapsi.fi/ohjeet/www-paivitys.html\n\nHaavoittuva sovellus löytyy hakemistostasi: ' + version_file + '\n\nSkannaus on suoritettu ' + timestamp + ' varmuuskopioista. Lisätietoja: ' + cve + '\n\nTähän sähköpostiin ei tarvitse vastata mikäli sinulla ei ole ongelmia päivityksessä. Huomaathan, että myös htaccessin takana olevat sivut suositellaan päivittämään ja keskeneräiset asennukset on asennettava loppuun tai poistettava. Mikäli verkkotunnuksesi on siirretty voit ajaa komennon "chmod 0220 hakemisto", jolloin emme lähetä sinulle enää muistutuksia.\n\nTerveisin,\n   Henri Salo\n   Kapsin ylläpito\n   yllapito@tuki.kapsi.fi\n\n   Kapsi Internet-käyttäjät ry\n   http://www.kapsi.fi/\n   http://tuki.kapsi.fi/', _charset='utf-8')
     try:
+        receivers = user.split(',')
         s = smtplib.SMTP_SSL(smtp_server, smtp_port)  # SMTP_SSL only works with Python 2.7
         username = getpass.getuser()
         password = getpass.getpass()
@@ -48,9 +47,9 @@ def send_email(user, appname, version_file, file_version, secure_version, cve):
         s.set_debuglevel(1)
         msg['Subject'] = 'Tietoturva-aukollinen sovellus löydetty sivuiltasi'
         msg['From'] = from_address
-        msg['To'] = to_address
+        msg['To'] = ", ".join(receivers)
         print msg
-        s.sendmail(from_address, [to_address], msg.as_string())
+        s.sendmail(from_address, receivers, msg.as_string())
         s.quit()
     except Exception:
         sys.exit(traceback.format_exc())
@@ -71,7 +70,7 @@ def read_csv(csv_file):
         counter = counter + 1
         if user and timestamp and appname and version_file and file_version and  secure_version and cve:
             print('[*] Processing %i line..' % counter)
-            send_email(user, appname, version_file, file_version, secure_version, cve)
+            send_email(user, timestamp, appname, version_file, file_version, secure_version, cve)
     print('\n[*] Processed %i notifications. Happy customer is a happy customer!' % counter)
 
 
