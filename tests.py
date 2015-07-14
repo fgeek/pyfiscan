02106802ad126e77c1dc1482163bede1cc23e2d7
Henri Salo <henri@nerv.fi>
2015-07-14 21:44:36 +0300

    Simplify test cases

--- tests.py
+++ tests.py
@@ -3,29 +3,15 @@ from pyfiscan import is_not_secure
 from database import Database
 
 class CompareVersions(unittest.TestCase):
-    """Testing that comparison of version numbers are correct."""
-    def test_version_pairs_1(self):
+    def test_version_pairs(self):
+        """Testing that comparison of version numbers are correct."""
         self.assertTrue(is_not_secure('2.1', '2.0.2'))
-
-    def test_version_pairs_2(self):
         self.assertTrue(is_not_secure('2.0.1', '2.0'))
-
-    def test_version_pairs_3(self):
         self.assertTrue(is_not_secure('2.0.1', '2.0.0'))
-
-    def test_version_pairs_4(self):
         self.assertFalse(is_not_secure('2.0.2', '2.0.2'))
-
-    def test_version_pairs_5(self):
         self.assertFalse(is_not_secure('2.0.1', '2.0.2'))
-
-    def test_version_pairs_6(self):
         self.assertFalse(is_not_secure('2.0', '2.0.2'))
-
-    def test_version_pairs_7(self):
         self.assertTrue(is_not_secure('2.0', '1.9.9'))
-
-    def test_version_pairs_wordpress_feedwordpress(self):
         self.assertTrue(is_not_secure('2015.0514', '2015.0426'))
 
 
